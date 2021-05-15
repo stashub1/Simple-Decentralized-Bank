49917be5a2bc94a187f184c0aed099baeabfbed8
Stanislav <s77734@gmail.com>
2021-05-15 12:51:02 +0300

    First commit

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -4,18 +4,24 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  //add minter variable
 
-  //add minter changed event
+    address public minter;
+    event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
-  }
+    constructor() public payable ERC20("Decentralized Bank Currency", "DCB") {
+        minter = msg.sender;
+    }
 
-  //Add pass minter role function
+    function passMinterRole(address newMinter) public returns (bool) {
+        require(msg.sender == minter, "Error. Only contract creator can assign new role");
+        minter = newMinter;
 
-  function mint(address account, uint256 amount) public {
-    //check if msg.sender have minter role
-		_mint(account, amount);
-	}
+        emit MinterChanged(msg.sender, newMinter);
+        return true;
+    }
+
+    function mint(address adr, uint amount) public {
+        require(msg.sender == minter, "Error. Minter only can be creator of token");
+        _mint(adr, amount);
+    }
 }
